05f8f2f4574e724ed5fa3ae9b02f2d612aa6ef4a
Nikolas Palm <nikolaspalm@gmail.com>
2017-11-07 10:29:58 +0100

    updated title

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>pcars2 Test</title>
+      <title>Highscore Board</title>
       <link href="css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
